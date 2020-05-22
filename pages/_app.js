bbd6218c2968d5947d8539bc857a626118491f5d
Jill Royer <hello@jillroyer.me>
2020-05-22 15:38:03 +0200

    fix: maybe fix huge icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "normalize.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-
+config.autoAddCss = false;
 import "../components/globalStyle.css";
 
 export const dataContext = React.createContext({});
