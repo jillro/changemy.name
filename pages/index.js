d49bed5976015ad74a2361aaf059781c9c72b4e5
Jill Royer <hello@jillroyer.me>
2020-06-06 00:04:24 +0200

    fix: push correct url on front page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import IndexPage, { getStaticProps as langGetStaticProps } from "./[lang]";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export async function getStaticProps(context) {
   return langGetStaticProps({ params: { lang: "en" }, ...context });
 }
 
-export default IndexPage;
+export default function Index(props) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/[lang]", "/en", { shallow: true });
+  });
+
+  return <IndexPage {...props} />;
+}
