3ac6a7d65bc122c3f0a136f546e64e85ab720498
Jill-Maud Royer <hello@jillroyer.me>
2022-06-27 18:40:49 +0200

    Updates

--- _next/static/G2TYBpnP5TAJqXPTesuGk/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F[lang]\u002F[company]","\u002F[lang]\u002Fabout\u002F[page]","\u002F[lang]\u002Fcompanies","\u002F[lang]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
